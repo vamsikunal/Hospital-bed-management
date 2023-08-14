c1982ce85272a8150295949a966916ff54147134
Kunal <kunal@pop-os.localdomain>
2023-08-14 10:15:07 +0530

    minor changes

--- app/hbms/settings.py
+++ app/hbms/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,9 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 
 # Default primary key field type
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # Run Django database migrations
 
 python3 manage.py makemigrations
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
 exec gunicorn hbms.wsgi:application --bind 0.0.0.0:8000 
